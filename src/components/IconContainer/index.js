780fdb777c78fa72d46d60e2e5ca30fbed84b070
FelipenKniess <felipe.nkniess@gmail.com>
2021-05-12 07:36:44 -0300

    Criado componente de icone dos containers

---
+++ src/components/IconContainer/index.js
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import {Container} from './style';
+
+const IconContainer = ({children}) => {
+    return(
+        <Container>
+            <div>
+                {children}
+            </div>
+        </Container>
+    )
+}
+
+export default IconContainer;
---
+++ src/components/IconContainer/style.js
@@ -0,0 +1,21 @@
+﻿import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 186px;
+    height: 186px;
+    border: 1px dashed #EC008C;
+    border-radius: 50%;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 130px;
+        height: 130px;
+        border-radius: 50%;
+        background: #FFF;
+    }
+`
