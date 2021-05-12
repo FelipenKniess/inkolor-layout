24442426f233c3e21044c8f7c26d5f0b49952e28
FelipenKniess <felipe.nkniess@gmail.com>
2021-05-12 08:01:01 -0300

    Criado componente de input

---
+++ src/components/Input/index.js
@@ -0,0 +1,49 @@
+﻿import React, { InputHTMLAttributes, useEffect, useRef, useCallback, useState} from 'react';
+import { Container, Error} from './styles';
+import {FiAlertCircle} from 'react-icons/fi';
+import { useField } from '@unform/core';
+
+const Input = ({name, ...rest}) => {
+   const inputRef = useRef(null);
+
+   const [isFilled, setIsFilled] = useState(false);
+   const [isFocused, setIsFocused] = useState(false);
+   const { fieldName, defaultValue, error, registerField } = useField(name);
+
+   useEffect(() => {
+       registerField({
+           name: fieldName,
+           ref: inputRef.current,
+           path: 'value'
+       })
+   }, [fieldName, registerField]);
+
+   const handleInputFocus = useCallback(() => {
+       setIsFocused(true);
+   }, []);
+
+   const handleInputBlur = useCallback(() => {
+       setIsFocused(false);
+       setIsFilled(!!inputRef.current?.value);
+   }, []);
+
+    return(
+       <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+           { Icon && <Icon size={20}/> }
+           <input
+               onFocus={handleInputFocus}
+               onBlur={handleInputBlur}
+               defaultValue={defaultValue}
+               ref={inputRef}
+               {...rest}
+           />
+           {error && (
+               <Error>
+                   <FiAlertCircle size={20} />
+               </Error>
+           )}
+       </Container>
+    )
+}
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,51 @@
+﻿﻿import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+    background: #232129;
+    display:flex;
+    width: 100%;
+    padding: 16px;
+    border-radius: 10px;
+    border: 2px solid #232129;
+    color: #666360;
+    ${(props) => props.isErrored &&
+        css`
+            border-color: #c53030;
+        `
+    }
+    > svg {
+        margin-right: 16px;
+        ${(props) => props.isFilled &&
+            css`
+                color: #ff9000;
+            `
+        }
+    }
+    ${(props) => props.isFocused &&
+        css`
+            border-color: #ff9000;
+            color: #ff9000;
+        `
+    }
+    input {
+        flex: 1;
+        background: transparent;
+        color: #fff;
+        border: 0;
+        &::placeholder {
+            color: #666360;
+        }
+    }
+`
+
+export const Error = styled.div`
+    height: 20px;
+    color: #c53030;
+    span {
+        background: #c53030;
+        color: #fff;
+        &::before {
+            border-color: #c53030 transparent;
+        }
+  }
+`
